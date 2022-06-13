c458f98419523ce7a2efa4c89783e8903b018f3b
pmielech <patryk.mielech@protonmail.com>
2022-06-13 23:24:19 +0200

    Removed database, determing based on points and area

--- rec.py
+++ rec.py
@@ -1,86 +1,109 @@
-import gc
-from xml.sax.handler import feature_string_interning
-import caer
-import canaro
-import os
-import cv2 as cv
-import matplotlib.pyplot as plt
-#import keras
-from keras.utils import np_utils
-from keras.callbacks import LearningRateScheduler
+from tkinter import Frame
+from cv2 import threshold
 import numpy as np
-#import scipy
+import cv2 as cv
+import time
+import matplotlib.pyplot as plt
+
+def empty():
+    pass
+
+def videoCapture():
+    capture = cv.VideoCapture(1)
+    isTrue, frame = capture.read()
+    return  frame
 
 
-img_size = (80, 80)
-channels = 1        #grey scale
-char_path = r'dataset\training'
+def getContours(img, imgContour):
+    contours, hierarchy = cv.findContours(img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+
+    for x in contours:
+        area = cv.contourArea(x)
+        if area > 2000:
+            cv.drawContours(imgContour, x, -1, (255, 0, 255), 7)    
+            peri = cv.arcLength(x, True)
+            approx = cv.approxPolyDP(x, 0.02 * peri, True)
+            x1, y1, x2, y2 = cv.boundingRect(approx)   # x, y, w, h
+            #if len(approx) 
+            print("x: " + str(x1+x2))
+            print("y: " + str(y1+y2))
+            print((x1+x2)/(y1+y2))
+            print((y1+y2)/(x1+x2))
+            cv.rectangle(imgContour, (x1, y1), (x1+x2, y1+y2), (0, 255, 0), 5)
+            cv.putText(imgContour, "Points: " + str(len(approx)), (x1 + x2 + 20, y1 + y2 + 20), cv.FONT_HERSHEY_COMPLEX, .7, (0, 255, 0), 2)
+            cv.putText(imgContour, "Area: " + str(int(area)), (x1 + x2 + 45, y1 + y2 + 45), cv.FONT_HERSHEY_COMPLEX, .7, (0, 255, 0), 2)
+            if len(approx) <= 7 and area > 4500:
+                cv.putText(imgContour, "Nutt",(x1 + x2 + 70, y1 + y2 + 70), cv.FONT_HERSHEY_COMPLEX, .7, (0, 255, 0), 2)
+            if len(approx) >= 6  and area > 7000:
+                cv.putText(imgContour, "Bolt",(x1 + x2 + 70, y1 + y2 + 70), cv.FONT_HERSHEY_COMPLEX, .7, (0, 255, 0), 2)
+            if len(approx) == 8:
+                cv.putText(imgContour, "Washer",(x1 + x2 + 70, y1 + y2 + 70), cv.FONT_HERSHEY_COMPLEX, .7, (0, 255, 0), 2)
+            
+
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv.cvtColor( imgArray[x][y], cv.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv.cvtColor(imgArray[x], cv.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+
+def back(*args):
+    pass
+
+cv.namedWindow("Main")
+cv.resizeWindow("Main", 640, 240)
+cv.createTrackbar("Threshold1", "Main", 255, 255, empty)
+cv.createTrackbar("Threshold2", "Main", 52, 255, empty)
 
 
-char_dict = {}
-for char in os.listdir(char_path):
-    char_dict[char] = len(os.listdir(os.path.join(char_path, char)))
 
-# sort
-char_dict = caer.sort_dict(char_dict, descending=True)
-print(char_dict)
+while True:
+    
 
-mech = []
-cnt = 0
-for i in char_dict:
-    mech.append(i[0])
-    cnt += 1
-    if cnt >= 4:
+    #if butt == True:
+    img = videoCapture()
+    imgContour = img.copy()
+
+    cv.waitKey(100)
+
+    blurred = cv.GaussianBlur(img, (7,7), 0)
+    imgGray = cv.cvtColor(blurred, cv.COLOR_BGR2GRAY)
+
+    threshold1 = cv.getTrackbarPos("Threshold1",  "Main")
+    threshold2 = cv.getTrackbarPos("Threshold2", "Main")
+
+    imgCanny = cv.Canny(imgGray, threshold1, threshold2)
+
+    kernel = np.ones((5,5))
+    imgDil = cv.dilate(imgCanny, kernel, iterations=1)
+
+    getContours(imgDil, imgContour)
+
+    imgStack = stackImages(0.8, ([img, imgDil, imgContour]))
+    cv.imshow("Result", imgStack)
+    if cv.waitKey(1) & 0xFF == ord('q'):
         break
-print(mech)
-
-# Creating the ttraining data
-train = caer.preprocess_from_dir(char_path, mech, channels=channels, IMG_SIZE=img_size, isShuffle=True)
-#plt.figure(figsize=(30,30))
-#plt.imshow(train[0][0], cmap='gray')
-#plt.show()
-
-featureSet, labels = caer.sep_train(train, IMG_SIZE=img_size)
-
-#normalize the featureSet ==> (0,1)
-featureSet = caer.normalize(featureSet)
-labels = np_utils.to_categorical(labels, len(mech))
-
-x_train, x_val, y_train, y_val = caer.train_val_split(featureSet, labels, val_ratio=.2)
-
-# freeing space
-#del train, featureSet, labels
-print(gc.collect())
-
-
-Batch_size = 32
-Epochs = 10
-
-#image data generator
-
-datagen = canaro.generators.imageDataGenerator()
-train_gen = datagen.flow(x_train, y_train, batch_size = Batch_size)
-
-#creating the model
-model = canaro.models.createSimpsonsModel(IMG_SIZE=img_size, channels=channels, output_dim=len(mech), loss='binary_crossentropy', decay=1e-6, learning_rate= 0.001, momentum=0.9, nesterov=True)
-
-#model.summary()
-
-callbacks_list = [LearningRateScheduler(canaro.lr_schedule)]
-training = model.fit(train_gen, steps_per_epoch = len(x_train)//Batch_size, epochs = Epochs, validation_data = (x_val, y_val), validation_steps = len(y_val)//Batch_size, callbacks = callbacks_list)
-
-test_path = r'dataset\training\washer\washer_noun_002_40578.jpg'
-img = cv.imread(test_path)
-
-def prepare(img):
-    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    img = cv.resize(img, img_size)
-    img = caer.reshape(img, img_size, channels=channels)
-    return img
-
-plt.imshow(img)
-plt.show()
-
-predictions = model.predict(prepare(img))
-
-print(mech[np.argmax(predictions[0])])
